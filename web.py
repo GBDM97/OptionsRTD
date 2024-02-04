e01f46144c6f8d4ba74bfc45b16412e13341f255
Ghiancarlo Morelli <ghiancarlo.morelli.externo@caixavidaeprevidencia.com.br>
2024-02-03 22:50:53 -0300

    Dry and Raw data pages added

--- web.py
+++ web.py
@@ -2,22 +2,37 @@ import streamlit as st
 import requests
 import pandas as pd
 import time
-import ast
 
 def colorFn(v):
     if v.name == 'Buy':
-        return ['color: blue'] * len(v)
+        return ['color: deepSkyBlue'] * len(v)
     if v.name == 'Sell':
         return ['color: red'] * len(v)
+    
+mode = st.selectbox(
+        "",
+        ("Lock","Dry", "Raw Data"),
+)
 
 with st.empty():
-    while True:
+   
+    while mode == 'Lock':
         data = requests.get("http://127.0.0.1:8000/options/").json()
-        df = pd.DataFrame(ast.literal_eval(data['optData']), columns=['','Buy','Sell','Index']
-        )
-        df = df.style.apply(
-            colorFn, subset=(slice(None),['Buy','Sell'])
-        )
+        df = pd.DataFrame(data['excerpt']['lockData'], columns=['Profit Price','Buy','Sell','Index'])
+        df = df.style.apply(colorFn, subset=(slice(None),['Buy','Sell'])
+        ).applymap(lambda x: 'color: lime' if float(x) <= 0.25 else None, subset=(slice(None),['Index']))
+        st.dataframe(df, hide_index=True, width=800, height=330)
+        time.sleep(10)
         
-        st.table(df) 
+    while mode == "Dry":
+        # Apply custom styles to the entire Streamlit app
+        data = requests.get("http://127.0.0.1:8000/options/").json()
+        df = pd.DataFrame(data['excerpt']['dryData'], columns=['Strike','Buy', 'Price']).style.apply(
+        colorFn, subset=(slice(None),['Buy']))
+        st.dataframe(df, hide_index=True, width=900, height=330)
+        time.sleep(10)
+    while mode == "Raw Data":
+        data = requests.get("http://127.0.0.1:8000/options/").json()
+        df = pd.DataFrame(data['excerpt']['optionsData'])
+        st.dataframe(df, hide_index=True, width=900, height=330)
         time.sleep(10)
